2f7177180d144f2f6dc5490c9d5beacdb0c6f623
Muhammad Safwan Karim <safwan_karim@diya-tech.com>
2023-11-06 12:59:19 +0500

    Moved main to Test

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,5 +1,3 @@
-package org.lucene.index.files;
-
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
@@ -11,13 +9,14 @@ import org.apache.lucene.search.MatchAllDocsQuery;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.tika.exception.TikaException;
+import org.lucene.index.files.FileIndexer;
 import org.xml.sax.SAXException;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 
-public class Main {
+public class Test {
     public static void main(String[] args) throws IOException, TikaException, SAXException {
         File sourceDirectory = Path.of("data", "data-source").toFile();
         Path indexPath = Path.of("data", "index");
